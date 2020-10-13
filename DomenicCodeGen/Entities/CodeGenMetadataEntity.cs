860f0a93e86c3f9df950de43637bcaa8bdc2344a
杨德明 <yalvhe2009@outlook.com>
2020-10-13 15:21:09 +0800

    初次提交

---
+++ DomenicCodeGen/Entities/CodeGenMetadataEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DM.DomenicCodeGen.Entities
+{
+    public class CodeGenMetadataEntity
+    {
+        /// <summary>
+        /// 模板的名字（文件名）
+        /// </summary>
+        public string TemplateFileName { get; set; }
+
+        public string FileContent { get; set; }
+
+        public string OutFileContent { get; set; }
+
+        /// <summary>
+        /// 变量,key为变量名字，value为：要把该变量替换为什么内容。
+        /// </summary>
+        public Dictionary<string,string> Variables { get; set; }
+
+        public string OutputPath { get; set; }
+    }
+}
